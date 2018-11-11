979eca9f7b8ea50609bdd70ef306475cdc4b9f41
JDLuke <jd@jdluke.com>
2018-11-11 18:58:19 -0500

    Added management endpoints using Spring Activator

--- src/main/java/com/oopuniversity/kafkapoc/App.java
+++ src/main/java/com/oopuniversity/kafkapoc/App.java
@@ -1,21 +1,12 @@
 package com.oopuniversity.kafkapoc;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.oopuniversity.kafkapoc.config.ConfigItem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.oopuniversity.kafkapoc")
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-
-        try {
-            System.out.println("Test JSON: " + new ObjectMapper().writeValueAsString(new ConfigItem("key","value")));
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
         SpringApplication.run(App.class, args);
     }
 }
--- src/main/java/com/oopuniversity/kafkapoc/appconfig/AppConfiguration.java
+++ src/main/java/com/oopuniversity/kafkapoc/appconfig/AppConfiguration.java
@@ -1,6 +1,8 @@
 package com.oopuniversity.kafkapoc.appconfig;
 
 import com.oopuniversity.kafkapoc.config.Config;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,6 +19,12 @@ public class AppConfiguration {
     @Bean
     Map<String, String> getAppConfiguration() {
         return new HashMap<>();
+    }
+
+    @Bean
+    public HealthIndicator helloHealthIndicator() {
+        return () -> Health.up().withDetail("config", "service").build();
+
     }
 
 }
